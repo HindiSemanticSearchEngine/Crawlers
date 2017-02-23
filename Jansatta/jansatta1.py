8de30ca3aeb0c09e4cc3751df20838d9f17dc089
DUSHYANT SINGH <dushyant7917@gmail.com>
2017-02-23 11:02:28 +0530

    Update jansatta1.py
    indentation error fixed

--- Jansatta/jansatta1.py
+++ Jansatta/jansatta1.py
@@ -2,14 +2,14 @@ import requests
 from lxml import html
 import time
 
-urls = []
+urls = [] 
 
 for i in xrange(315) :
-	page = requests.get("http://www.jansatta.com/latest-news/page/"+str(i)+"/?s=news")
-	time.delay(5)
-	tree = html.fromstring(page.content)
-	article_urls = tree.xpath('//div[@class="newslistbx"]/h3/a/@href')
-	for j in article_urls:
+    page = requests.get("http://www.jansatta.com/latest-news/page/"+str(i)+"/?s=news")
+    time.delay(5)
+    tree = html.fromstring(page.content)
+    article_urls = tree.xpath('//div[@class="newslistbx"]/h3/a/@href')
+    for j in article_urls:
     	urls.append(j)
     
 for k in urls:

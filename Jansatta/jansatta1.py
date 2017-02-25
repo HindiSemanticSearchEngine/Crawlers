3395148b13f9e617ac0c5bb1d685929e842933c4
Anoop96 <anoop96tomar@gmail.com>
2017-02-25 11:03:22 +0530

    change in time delay

--- Jansatta/jansatta1.py
+++ Jansatta/jansatta1.py
@@ -6,7 +6,7 @@ urls = []
 
 for i in xrange(315) :
     page = requests.get("http://www.jansatta.com/latest-news/page/"+str(i)+"/?s=news")
-    time.delay(5)
+    time.sleep(5)
     tree = html.fromstring(page.content)
     article_urls = tree.xpath('//div[@class="newslistbx"]/h3/a/@href')
     for j in article_urls:
@@ -23,7 +23,7 @@ for k in urls:
 
     #collection.insert({"url":page_url, "title":title})
     print(title)
-    time.delay(5)
+    time.sleep(5)
 
 
 

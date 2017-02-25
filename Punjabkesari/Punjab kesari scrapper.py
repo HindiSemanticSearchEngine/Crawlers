8082a5737ccbab8ad6395cc01359cf2d493cdd49
Ritvikjain <ritvikjain2@gmail.com>
2017-02-25 13:14:01 +0530

    Update Punjab kesari scrapper.py

--- Punjabkesari/Punjab kesari scrapper.py
+++ Punjabkesari/Punjab kesari scrapper.py
@@ -1,19 +1,24 @@
 
+
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
 
 
 
-for i in range(1,1000):
+for i in range(1,2000):
     r=requests.get("http://www.punjabkesari.in/latest.aspx?pageno=" + str(i))
     soup = BeautifulSoup(r.content,"html.parser")
     for j in soup.find_all("h2"):
         for k in j.find_all("a"):
-            print(k.text)        
-    for title in soup.find_all("h2"):
-        for url in title.find_all("a"):
-            print(url.get('href'))
-    for img in soup.find_all('span'):
-        for img1 in img.find_all('img'):
-            print(img1.get('data-original'))
+            url=(k.get('href'))
+            r=requests.get(url)
+            soup=BeautifulSoup(r.content,"html.parser")
+            for l in soup.find_all('div',class_="story-part"):
+                for g in l.find_all("h2"):
+                    print(g.text)
+            for keywords in soup.find_all("head"):
+                for key in keywords.find_all("meta"):
+                                          print(key.get("content"))
+                for link in keywords.find_all("link"):
+                    print(link.get('href'))
